f52778f76c385f7b81c9d2efef1acc6a8ddfcf84
Gibexz <andrewetedu@gmail.com>
2023-10-25 19:44:01 +0100

    Task 1. FIFO caching v2

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -19,15 +19,13 @@ class FIFOCache(BaseCaching):
         """
         FIFO put method
         """
-        if key is None or item is None:
-            pass
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = next(iter(self.cache_data))
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
 
-        self.cache_data[key] = item
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            print(f"Discard: {list(self.cache_data.keys())[0]}")
-            discard = list(self.cache_data.keys())[0]
-            self.cache_data.pop(discard)
+            self.cache_data[key] = item
 
     def get(self, key):
         """
