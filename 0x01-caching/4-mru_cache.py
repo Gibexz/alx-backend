28ff95a5b2d2b4089933743cf67420084c31a805
Gibexz <andrewetedu@gmail.com>
2023-10-25 23:33:31 +0100

    Task 4. MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+module: 4-mru_cache.py
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    LRU policiy caching method
+    """
+    def __init__(self):
+        """
+        init command
+        """
+        super().__init__()
+        self.lruKeys = []
+
+    def put(self, key, item):
+        """
+        MRU put method
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data.keys():
+                self.cache_data[key] = item
+                return
+
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = self.lruKeys.pop(-1)
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+
+            self.cache_data[key] = item
+            """update the lru key tracking list"""
+            self.lruKeys.append(key)
+
+    def get(self, key):
+        """
+        MRU get method
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        """When key is accessed, it is saved to the end of the list
+        as most recent"""
+        self.lruKeys.remove(key)
+        self.lruKeys.append(key)
+        return self.cache_data[key]
