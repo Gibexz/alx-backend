3fc08bb868584336b544495d746708d53bc2e165
Gibexz <andrewetedu@gmail.com>
2023-10-31 22:46:44 +0100

    Task 1. Basic Babel setup fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,26 +4,14 @@ module: 0-app.py
 """
 
 from flask import Flask, render_template
-from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel(app)
-
-
-class Config:
-    """bable config"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
 
 
 @app.route("/")
 def hello():
     """a basic flask route"""
-    return render_template('1-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
