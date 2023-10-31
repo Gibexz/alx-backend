ed5d9fc843425a8033e9fba6747ee9948d6f7406
Gibexz <andrewetedu@gmail.com>
2023-11-01 00:41:07 +0100

    Task 3. Parametrize templates and Task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -12,7 +12,7 @@ babel = Babel(app)
 
 
 class Config:
-    """bable config"""
+    """bable config code"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
