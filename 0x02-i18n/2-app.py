5303550c075ee52aaf40b16f39debec24d94dc25
Gibexz <andrewetedu@gmail.com>
2023-10-31 23:23:58 +0100

    Task 2. Get locale from request v1

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -23,6 +23,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    """auto language selector"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
