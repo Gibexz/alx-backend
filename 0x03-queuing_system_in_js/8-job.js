af1eef600a3c2569a907359363baebdba7b04923
Gibexz <andrewetedu@gmail.com>
2023-11-29 12:55:35 +0100

    Task 10. Writing the job creation function

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,27 @@
+const kue = require('kue');
+
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (Array.isArray(jobs)) {
+    jobs.forEach((jobObject) => {
+      const newJob = queue.create('push_notification_code_3', jobObject);
+      newJob
+        .on('enqueue', () => {
+          console.log(`Notification job created: ${newJob.id}`);
+        })
+        .on('complete', () => {
+          console.log(`Notification job ${newJob.id} completed`);
+        })
+        .on('failed', (error) => {
+          console.log(`Notification job ${newJob} failed: ${error}`);
+        })
+        .on('progress', (progress, data) => {
+          console.log(`Notification job ${newJob} ${progress}% complete`);
+        });
+      newJob.save();
+    });
+  } else {
+    throw (new Error('Jobs is not an array'));
+  }
+};
+
+module.exports = createPushNotificationsJobs;
